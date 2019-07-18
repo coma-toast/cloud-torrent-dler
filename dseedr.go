32dd24b4b8291227e2c3083f493485022b2bb3e0
Jason <jdale215@gmail.com>
2019-07-18 15:35:20 -0400

    api call works

--- dseedr.go
+++ dseedr.go
@@ -3,13 +3,11 @@ package main
 // Lmo2~C}8fDJ%yj,CpfUv
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 type RootFolder struct {
@@ -27,7 +25,7 @@ type RootFolder struct {
 }
 
 type Folder struct {
-	space_max       int
+	"space max"       int
 	space_used      int
 	code            int
 	timestamp       string
@@ -51,15 +49,12 @@ type File struct {
 	stream_video bool
 }
 
-// Client struct is the http Client
+// Client struct is the http Client struct
 type Client struct {
-	// TODO: figure out this (aka increase my Go knowledge)
-	// BaseURL    *url.URL
-	Domain     string
-	UserAgent  string
-	UserName   string
-	Password   string
-	httpClient *http.Client
+	Domain    string
+	UserAgent string
+	UserName  string
+	Password  string
 }
 
 // Service is the service
@@ -68,113 +63,23 @@ type Service interface {
 }
 
 func main() {
-	response := callAndUnmarshal("GET", "folder", response)
-	fmt.Printf(response)
-	// response, err := http.Get("https://www.seedr.cc/rest/folder/")
-	// if err != nil {
-	// 	fmt.Printf("HTTP request failed with error %s\n", err)
-	// } else {
-	// 	data, _ := ioutil.ReadAll(response.Body)
-	// 	fmt.Println(string(data))
-	// }
-
-	// jsonData := map[string]string{"username": "jdale215@gmail.com", "password": "Lmo2~C}8fDJ%yj,CpfUv", "id": "95429980"}
-	// jsonValue, _ := json.Marshal(jsonData)
-	// response, err = http.Post("https://www.seedr.cc/rest/folder/", "application/json", bytes.NewBuffer(jsonValue))
-	// if err != nil {
-	// 	fmt.Printf("HTTP request failed with error %s\n", err)
-	// } else {
-	// 	data, _ := ioutil.ReadAll(response.Body)
-	// 	fmt.Println(string(data))
-	// }
+	// response := callAndUnmarshal("GET", "folder", response)
+	root := getRootFolder()
+	spew.Dump(root)
 }
 
-func (c Client) call(method string, url string) ([]byte, error) {
-	fullURL := fmt.Sprintf("https://%s%s", c.Domain, url)
-
-	req, err := http.NewRequest("GET", fullURL, nil)
+func getRootFolder() RootFolder {
+	var username string = "jdale215@gmail.com"
+	var passwd string = "Lmo2~C}8fDJ%yj,CpfUv"
+	jsonData := RootFolder{}
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", "https://www.seedr.cc/rest/folder/", nil)
 	if err != nil {
-		return []byte{}, err
+		spew.Dump(err)
 	}
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	return body, nil
-}
-
-func (c Client) callAndUnmarshal(method string, url string, target interface{}) error {
-	response, err := c.call(method, url)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(response, target)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// ? From a tutorial. Not sure how useful this is...
-func (c *Client) ListFolder() ([]Folder, error) {
-	req, err := c.newRequest("GET", "/folder", nil)
-	if err != nil {
-		return nil, err
-	}
-	var folder []Folder
-	_, err = c.do(req, &folder)
-	return folder, err
-}
-
-func (c *Client) ListFiles(id int) ([]File, error) {
-	path := fmt.Sprintf("/folder/%d", id)
-	req, err := c.newRequest("GET", path, nil)
-	if err != nil {
-		return nil, err
-	}
-	var file []File
-	_, err = c.do(req, &file)
-	return file, err
-}
-
-func (c *Client) newRequest(method, path string, body interface{}) (*http.Request, error) {
-	rel := &url.URL{Path: path}
-	u := c.BaseURL.ResolveReference(rel)
-	var buf io.ReadWriter
-	if body != nil {
-		buf = new(bytes.Buffer)
-		err := json.NewEncoder(buf).Encode(body)
-		if err != nil {
-			return nil, err
-		}
-	}
-	req, err := http.NewRequest(method, u.String(), buf)
-	if err != nil {
-		return nil, err
-	}
-	if body != nil {
-		req.Header.Set("Content-Type", "application/json")
-	}
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("User-Agent", c.UserAgent)
-	return req, nil
-}
-func (c *Client) do(req *http.Request, v interface{}) (*http.Response, error) {
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	err = json.NewDecoder(resp.Body).Decode(v)
-	return resp, err
+	request.SetBasicAuth(username, passwd)
+	response, err := client.Do(request)
+	data, _ := ioutil.ReadAll(response.Body)
+	json.Unmarshal(data, &jsonData)
+	return jsonData
 }
