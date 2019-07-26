a366c2182c9d1df1510df56541f417bc9e378199
Jason <jdale215@gmail.com>
2019-07-26 08:51:32 -0400

    wip

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ type Folder struct {
 	Result  bool     `json:"result"`
 }
 
+// SubFolder is a folder in folder - a sort of, Folder Inception, if you will
 type SubFolder struct {
 	ID         int    `json:"id"`
 	Name       string `json:"name"`
@@ -38,6 +39,7 @@ type SubFolder struct {
 	LastUpdate string `json:"last_update"`
 }
 
+// File is the file data object. Use the ID to get the file itself
 type File struct {
 	ID             int    `json:"id"`
 	Name           string `json:"name"`
@@ -47,6 +49,7 @@ type File struct {
 	StreamAudio    bool   `json:"stream_audio"`
 	StreamVideo    bool   `json:"stream_video"`
 	VideoConverted string `json:"video_converted,omitempty"`
+	ParentFolder   int
 }
 
 // Client struct is the http Client struct
@@ -62,18 +65,35 @@ type Service interface {
 	// GetCustomer(id int) (Customer, error)
 }
 
+// BaseURL is the first part of the REST url
 var BaseURL = "https://www.seedr.cc/rest"
+
+// DlRoot is the download directory
 var DlRoot = "./tmp"
+
+//Username is username
 var Username = "jdale215@gmail.com"
+
+// Passwd is the password
 var Passwd = "Lmo2~C}8fDJ%yj,CpfUv"
+
+// Credentials is the base64 encoded username:password
 var Credentials = b64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", Username, Passwd)))
 
 func main() {
-	files := getFilesFromFolder(0)
-	// spew.Dump(files)
-	downloadFiles(files)
+	// Start at the root folder, recursively searching down, populating the files list
+	files := getFilesFromFolder(96452508)
+	doNothing(files)
+	// downloadFiles(files)
 }
 
+func doNothing(nothing interface{}) {
+	spew.Dump(nothing)
+	return
+}
+
+// Simple api call method for gathering info - NOT the actual downloading of the file.
+// callType is 'file' or 'folder'
 func apiCall(method string, id int, callType string) []byte {
 	url := fmt.Sprintf("%s/%s", BaseURL, callType)
 	if id != 0 {
@@ -91,25 +111,31 @@ func apiCall(method string, id int, callType string) []byte {
 	return data
 }
 
+// Get folder info
 func getFolder(id int) Folder {
 	var rootData Folder
 	data := apiCall("GET", id, "folder")
-
 	err := json.Unmarshal(data, &rootData)
 	handleError(err)
 	return rootData
 }
 
+// Get the files in the folder and any subfolders
 func getFilesFromFolder(folderID int) []File {
 	folder := getFolder(folderID)
 	files := folder.Files
 	for _, folder := range folder.Folders {
 		subfiles := getFilesFromFolder(folder.ID)
+		for _, subfile := range subfiles {
+			subfile.ParentFolder = folder.ID
+		}
+		// spew.Dump("subFiles: ", subfiles)
 		files = append(files, subfiles...)
 	}
 	return files
 }
 
+// Do the actual download of the files
 func downloadFiles(files []File) {
 	for _, file := range files {
 		isAVideo, _ := regexp.MatchString("(.*?).(jpg|txt)$", file.Name)
