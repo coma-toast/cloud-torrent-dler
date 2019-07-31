d2cf624c93b26af1f2e4a256824b693af3f0c481
Jason <jdale215@gmail.com>
2019-07-31 09:45:19 -0400

    script for cron

---
+++ cdt.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+
+/usr/local/go/bin/go run main.go
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"regexp"
@@ -16,6 +17,7 @@ import (
 
 	"github.com/cavaliercoder/grab"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/spf13/viper"
 )
 
 // Folder is a folder that contains subfolders or files, or both
@@ -54,32 +56,17 @@ type File struct {
 	ParentFolder   int
 }
 
-// Client struct is the http Client struct
-type Client struct {
-	Domain    string
-	UserAgent string
-	UserName  string
-	Password  string
-}
-
-// Service is the service
-type Service interface {
-	// GetCustomer(id int) (Customer, error)
-}
-
 // BaseURL is the first part of the REST url
-var BaseURL = "https://www.seedr.cc/rest"
+var BaseURL string
 
 // DlRoot is the download directory
-// dev code
-// var DlRoot = "./tmp"
-var DlRoot = "/media/jason/3C72B82272B7DF38/Movies/NotKids"
+var DlRoot string
 
-//Username is username
-var Username = "jdale215@gmail.com"
+// Username is username
+var Username string
 
 // Passwd is the password
-var Passwd = "Lmo2~C}8fDJ%yj,CpfUv"
+var Passwd string
 
 // Credentials is the base64 encoded username:password
 var Credentials = b64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", Username, Passwd)))
@@ -98,38 +85,56 @@ func alreadyRunning(pidFile string) bool {
 				if err := process.Signal(syscall.Signal(0)); err == nil {
 					fmt.Println("PID already running!")
 					// We only get an error if the pid isn't running, or it's not ours.
-					fmt.Errorf("pid already running: %d", pid)
+					err := fmt.Errorf("pid already running: %d", pid)
+					log.Print(err)
 					return true
-				} else {
-					fmt.Println(err)
 				}
+				log.Print(err)
+
 			} else {
-				fmt.Println(err)
+				log.Print(err)
 			}
 		} else {
-			fmt.Println(err)
+			log.Print(err)
 		}
 	} else {
-		fmt.Println(err)
+		log.Print(err)
 	}
 	// If we get here, then the pidfile didn't exist,
 	// or the pid in it doesn't belong to the user running this app.
 	ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d", os.Getpid())), 0664)
-	// TODO: dev code
+	// * dev code
 	// ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d", 29355)), 0664)
 	return false
 }
 
 func main() {
 	pid := alreadyRunning("cloud-torrent-downloader")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		handleError(err)
+	}
+	BaseURL = viper.GetString("BaseURL")
+	DlRoot = viper.GetString("DlRoot")
+	Username = viper.GetString("Username")
+	Passwd = viper.GetString("Passwd")
+
+	// Credentials is the base64 encoded username:password
+	var Credentials = b64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", Username, Passwd)))
+
+	// DeleteQueue is a list of folders to delete once all the downloads have completed.
+	// Do not delete them right when the download completes, as there could be multiple files in a folder
+	var DeleteQueue []int
+
 	if !pid {
-		// Start at the root folder of your choosing (Completed),
+		// Start at the root folder of your choosing (i.e. Completed),
 		// recursively searching down, populating the files list
+		// TODO: make this an argument
 		files := getFilesFromFolder(96452508)
-		// files := getFilesFromFolder(0)
-		// doNothing(files)
 		downloadFiles(files)
-		// spew.Dump("delete queue: ", DeleteQueue)
+		// *
 		deleteDownloaded(DeleteQueue)
 	}
 }
@@ -178,16 +183,7 @@ func getFolder(id int) Folder {
 func getFilesFromFolder(folderID int) []File {
 	folder := getFolder(folderID)
 	files := folder.Files
-	for _, file := range files {
-		DeleteQueue = append(DeleteQueue, folder.ID)
-		// file.ParentFolder = folder.ID
-		fmt.Println(" ------- ")
-		fmt.Println("Folder: ", folder.ID)
-		fmt.Println("File: ", file.ID)
-		fmt.Println("Name: ", file.Name)
-		fmt.Println("Parent: ", file.ParentFolder)
-	}
-	// spew.Dump("subFiles: ", subfiles)
+	DeleteQueue = append(DeleteQueue, folder.ID)
 	for _, folder := range folder.Folders {
 		subfiles := getFilesFromFolder(folder.ID)
 		files = append(files, subfiles...)
@@ -198,6 +194,7 @@ func getFilesFromFolder(folderID int) []File {
 // Do the actual download of the files
 func downloadFiles(files []File) {
 	for _, file := range files {
+		// * dev code
 		// isAVideo, _ := regexp.MatchString("(.*?).(txt|jpg)$", file.Name)
 		isAVideo, _ := regexp.MatchString("(.*?).(mkv|mp4|avi)$", file.Name)
 		if isAVideo {
@@ -209,11 +206,9 @@ func downloadFiles(files []File) {
 			defer out.Close()
 
 			client := grab.NewClient()
-			// client.HTTPClient.Transport.DisableCompression = true
 
 			req, err := grab.NewRequest(path, fileURL)
 			handleError(err)
-			// ...
 			req.NoResume = true
 			req.HTTPRequest.Header.Set("Authorization", "Basic "+Credentials)
 			resp := client.Do(req)
