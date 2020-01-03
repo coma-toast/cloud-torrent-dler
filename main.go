7daf56ad28f5310661bf140c18954a0e1d5cc5bc
Jason <jdale215@gmail.com>
2020-01-02 20:25:18 -0500

    wip

--- main.go
+++ main.go
@@ -79,8 +79,8 @@ func main() {
 	// TODO: worker pools for downloading - they take a long time and setting a limit would be good
 
 	// downloadWorker()
-	for range time.NewTicker(time.Second * 5).C {
-		// for range time.NewTicker(time.Minute * 1).C {
+	// for range time.NewTicker(time.Second * 5).C {
+		for range time.NewTicker(time.Minute * 1).C {
 		for _, downloadFolder := range conf.CompletedFolder {
 			list, err := findAllToDownload(selectedSeedr, downloadFolder, conf.UseFTP)
 			if err != nil {
