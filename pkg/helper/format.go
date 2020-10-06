6f0359cb1cca70b22e2c38964138274d661852fa
Jason <jdale215@gmail.com>
2020-10-06 15:34:38 -0400

    format webrip and web-dl, and tests for formatting

--- pkg/helper/format.go
+++ pkg/helper/format.go
@@ -6,10 +6,15 @@ import (
 	"github.com/kennygrant/sanitize"
 )
 
+// SanitizeText eliminates unnecessary formatting.
 func SanitizeText(input string) string {
 	output := sanitize.BaseName(input)
 	output = strings.ReplaceAll(output, "-", " ")
 	output = strings.ToLower(output)
+	// For some reason, a lot of file names don't quite match the magnet name - WEBRip in the magnet name is changed to webdl in the file name.
+	output = strings.ReplaceAll(output, "webrip", "")
+	output = strings.ReplaceAll(output, "web dl", "")
+	output = strings.ReplaceAll(output, "  ", " ")
 
 	return output
 }
