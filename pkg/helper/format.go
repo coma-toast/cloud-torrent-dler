144f222457851651b75d594403bde3c18d0bc7e6
Jason <jdale215@gmail.com>
2020-10-07 09:01:42 -0400

    h264

--- pkg/helper/format.go
+++ pkg/helper/format.go
@@ -11,10 +11,13 @@ func SanitizeText(input string) string {
 	output := sanitize.BaseName(input)
 	output = strings.ReplaceAll(output, "-", " ")
 	output = strings.ToLower(output)
-	// For some reason, a lot of file names don't quite match the magnet name - WEBRip in the magnet name is changed to webdl in the file name.
+	// For some reason, a lot of file names don't quite match the magnet name - WEBRip in the magnet name is changed to webdl in the file name. Why?
 	output = strings.ReplaceAll(output, "webrip", "")
 	output = strings.ReplaceAll(output, "web dl", "")
+	// " -" now becomes "  " which is not right. Make it single space
 	output = strings.ReplaceAll(output, "  ", " ")
+	// Sometimes it's H.264, sometimes it's H264. Why??
+	output = strings.ReplaceAll(output, "h 264", "h264")
 
 	return output
 }
